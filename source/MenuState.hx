086b7a84feb533ec8dd6631b742c1bf8eb6d4d92
ashes999 <ashes999@yahoo.com>
2014-12-10 16:35:48 -0500

    Basic titlescreen.

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -13,15 +13,19 @@ import flixel.util.FlxMath;
 class MenuState extends FlxState
 {
 	/**
-	 * Function that is called up when to state is created to set it up. 
+	 * Function that is called up when to state is created to set it up.
 	 */
 	override public function create():Void
 	{
+    var sprite:FlxSprite = new FlxSprite('assets/images/ui/title.png');
+    add(sprite);
+    sprite.x  = (FlxG.width - sprite.width) / 2;
+    sprite.y = (FlxG.height - sprite.height) / 2;
 		super.create();
 	}
-	
+
 	/**
-	 * Function that is called when this state is destroyed - you might want to 
+	 * Function that is called when this state is destroyed - you might want to
 	 * consider setting all objects this state uses to null to help garbage collection.
 	 */
 	override public function destroy():Void
@@ -35,5 +39,5 @@ class MenuState extends FlxState
 	override public function update():Void
 	{
 		super.update();
-	}	
+	}
 }
